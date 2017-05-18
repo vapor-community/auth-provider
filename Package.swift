7141cc046c71bbca22034c884b57b7d21bde0a95
tanner0101 <me@tanner.xyz>
2017-05-18 19:39:58 +0100

    one

--- Package.swift
+++ Package.swift
@@ -4,9 +4,9 @@ let package = Package(
     name: "AuthProvider",
     dependencies: [
         // A web framework and server for Swift that works on macOS and Ubuntu. 
-        .Package(url: "https://github.com/vapor/auth.git", Version(1,0,0, prereleaseIdentifiers: ["beta"])),
+        .Package(url: "https://github.com/vapor/auth.git", majorVersion: 1),
 
         // A web framework and server for Swift that works on macOS and Ubuntu. 
-        .Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0, prereleaseIdentifiers: ["beta"]))
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
     ]
 )
